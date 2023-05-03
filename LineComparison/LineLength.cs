f5d835fa143c13274e42067b07f82ccdf56affd6
MD HUSSAIN <mdhussainrukhsadsiddiqui@gmail.com>
2023-05-03 18:20:32 +0530

    Welcome to Line Comparison Program

---
+++ LineComparison/LineLength.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LineComparison
+{
+    internal class LineLength
+    {
+        /*
+        public static void Calculation()
+        {
+
+            double a1, b1, a2, b2;
+
+            Console.WriteLine("Enter the (x,y) coordinates of the first point:");
+            a1 = double.Parse(Console.ReadLine());
+            b1 = double.Parse(Console.ReadLine());
+
+            Console.WriteLine("Enter the (x,y) coordinates of the second point:");
+            a2 = double.Parse(Console.ReadLine());
+            b2 = double.Parse(Console.ReadLine());
+
+            double length = Math.Sqrt(Math.Pow(a2 - a1, 2) + Math.Pow(b2 - b1, 2));
+
+            Console.WriteLine($"The length of the line connecting ({a1},{b1}) and ({a2},{b2}) is {length}");
+        
+
+        }
+        */
+    }
+}
---
+++ LineComparison/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LineComparison
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to my Program of Line Comparison Problem:");
+            //LineLength.Calculation();
+            Console.ReadKey();
+        }
+    }
+}
