4987f260c913b6a18e714fe520553aeb0abf6249
MD HUSSAIN <mdhussainrukhsadsiddiqui@gmail.com>
2023-05-04 06:03:22 +0530

    Checking if two lines are equal or not by using equal() method

--- LineComparison/LineLengthEquality.cs
+++ LineComparison/LineLengthEquality.cs
@@ -8,36 +8,42 @@ namespace LineComparison
 {
     public class LineLengthEquality
     {
-       /* public static void Check()
+        
+        public static void Check()
         {
+            Console.WriteLine("Enter the (x,y) coordinates of the first point of line 1:");
+            double x1 = double.Parse(Console.ReadLine());
+            double y1 = double.Parse(Console.ReadLine());
 
+            Console.WriteLine("Enter the (x,y) coordinates of the second point of line 1:");
+            double x2 = double.Parse(Console.ReadLine());
+            double y2 = double.Parse(Console.ReadLine());
 
-            double x1, y1, x2, y2;
+            Console.WriteLine("Enter the (x,y) coordinates of the first point of line 2:");
+            double x3 = double.Parse(Console.ReadLine());
+            double y3 = double.Parse(Console.ReadLine());
 
-            Console.WriteLine("Enter the (x,y) coordinates of the first point:");
-            x1 = double.Parse(Console.ReadLine());
-            y1 = double.Parse(Console.ReadLine());
+            Console.WriteLine("Enter the (x,y) coordinates of the second point of line 2:");
+            double x4 = double.Parse(Console.ReadLine());
+            double y4 = double.Parse(Console.ReadLine());
 
-            Console.WriteLine("Enter the (x,y) coordinates of the second point:");
-            x2 = double.Parse(Console.ReadLine());
-            y2 = double.Parse(Console.ReadLine());
+            double length1 = Math.Sqrt(Math.Pow(x2 - x1, 2) + Math.Pow(y2 - y1, 2));
+            double length2 = Math.Sqrt(Math.Pow(x4 - x3, 2) + Math.Pow(y4 - y3, 2));
+            Console.WriteLine("The length of first line = " + length1);
+            Console.WriteLine("The length of Second Line =" +length2);
+            int comparisonResult = Convert.ToInt32 (length1.Equals(length2));
 
-            double length = Math.Sqrt(Math.Pow(x2 - x1, 2) + Math.Pow(y2 - y1, 2));
-
-            Console.WriteLine($"The length of the line connecting ({x1},{y1}) and ({x2},{y2}) is {length}");
-            LineLengthEquality.LineEqualityChecker(x1, y1, x2, y2);
-        } */
-        public static void LineEqualityChecker(double x1, double y1, double x2, double y2)
-        {
-
-            if ((x1 == y1 && y2 == x2) || (x1 == x2 && y1 == y2))
+            if (comparisonResult == 1)
             {
-                Console.WriteLine("The two lines are equal.");
+                Console.WriteLine("Line 1 and Line2 are equal");
             }
             else
             {
-                Console.WriteLine("The two lines are not equal.");
+                Console.WriteLine("Line 1 and Line2 are not equal");
             }
+
+
+
         }
 
     }       
--- LineComparison/Program.cs
+++ LineComparison/Program.cs
@@ -11,7 +11,7 @@ namespace LineComparison
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome to my Program of Line Comparison Problem:");
-            LineLength.Calculation();
+            LineLengthEquality.Check();
            
             Console.ReadKey();
         }
